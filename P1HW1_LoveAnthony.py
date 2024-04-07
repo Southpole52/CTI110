8d780bd5a55c2e62ca1e5af9bf1bbe9b983e24ff
Southpole52 <166321775+Southpole52@users.noreply.github.com>
2024-04-07 13:16:03 -0400

    Add files via upload
    Second Submission

---
+++ P1HW1_LoveAnthony.py
@@ -0,0 +1,14 @@
+#Anthony Love
+#04/07/2024
+#P1HW1_LoveAnthony
+# A simple input and output with integers
+
+print('Enter integer:' , end = " ")
+int1 = int(input())
+print("You entered " , int1)
+print (int1 , "squared is "  , int1*int1)
+print("and" , int1 , "cubed is" , int1*int1*int1 , "!!")
+print("Enter another integer")
+int2 = int(input())
+print(int1 , "+" , int2  , "is" , int1+int2)
+print(int1 , "*" , int2  , "is" , int1*int2)
