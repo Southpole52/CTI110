7aeaab8abe17f35a6857d356b115bbbba4871157
Southpole52 <166321775+Southpole52@users.noreply.github.com>
2024-04-07 12:56:32 -0400

    Add files via upload

---
+++ P1Lab1_LoveAnthony.py
@@ -0,0 +1,12 @@
+# Anthony Love
+# 4/7/2024
+#P1Lab1_LoveAnthony
+# Simple Input and Output lab for the basics of python.
+
+
+
+print("Enter your first name:" , end = " ")
+first = input()
+print("Enter your last name:" , end = " ")
+last = input()
+print("Hello, " , first , last,"! Welcome to CTI-110 ")
